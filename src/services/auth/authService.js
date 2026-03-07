c9e7c34ce66edfc90a3251f36e46ec3e7eef0bb7
Afzalbek <gofurovafzalbek7@gmail.com>
2026-03-07 20:33:13 +0500

    fix register bug

--- src/services/auth/authService.js
+++ src/services/auth/authService.js
@@ -1,8 +1,12 @@
 import api from "../api/apiClient.js";
 import { endpoints } from "../api/endpoints.js";
 
+const tenant = window.location.pathname.split("/")[1];
 export const authService = {
 	login: (data) => api.post(endpoints.LOGIN, data).then((r) => r.data),
-	register: (data) => api.post(endpoints.REGISTER, data).then((r) => r.data),
+	register: (data) =>
+		api
+			.post(`${endpoints.REGISTER}?tenant=${tenant}`, data)
+			.then((r) => r.data),
 	me: () => api.get("/auth/me").then((r) => r.data),
 };
